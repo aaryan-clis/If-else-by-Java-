39fe4c2933f13a2a6116c485d2dda3f67f39c0ef
Aaryan <72549723+aaryan-clis@users.noreply.github.com>
2022-08-19 20:26:54 +0530

    Initial

---
+++ class verify{.java
@@ -0,0 +1,13 @@
+class verify{
+    public static void main (String args[])
+    {
+        int z = 10; 
+        if(z==10)
+        {
+            System.out.println("The value is correcct");
+        }
+        else{
+            System.out.println("Number is wrong ");
+        }
+    }
+}
